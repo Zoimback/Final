abe15569eb25490ad2d937a8265d5ee73367f748
Alex <ege@gmail.com>
2021-05-20 11:09:35 +0200

    Test1 ok

--- 6653/test/operaciones/OperacionesTest.java
+++ 6653/test/operaciones/OperacionesTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -40,48 +41,94 @@ public class OperacionesTest {
     /**
      * Test of sumarPar method, of class Operaciones.
      */
+    
     @Test
     public void testSumarPar() {
         System.out.println("sumarPar");
+        Operaciones instance = new Operaciones();
+        int a = 2;
+        int b = 2;
+        
+        int expResult = 4;
+        int result = instance.sumarPar(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }@Ignore
+    @Test
+    public void testSumarImPar() {
+        System.out.println("sumarPar");
+        Operaciones instance = new Operaciones();
         int a = 0;
         int b = 0;
-        Operaciones instance = new Operaciones();
+        
         int expResult = 0;
         int result = instance.sumarPar(a, b);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of mayor method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testMayor() {
         System.out.println("mayor");
+        Operaciones instance = new Operaciones();
         int a = 0;
         int b = 0;
-        Operaciones instance = new Operaciones();
+        
         int expResult = 0;
         int result = instance.mayor(a, b);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       
+    }
+      @Ignore
+    @Test
+    public void testMayorb() {
+        System.out.println("mayor");
+        Operaciones instance = new Operaciones();
+        int a = 0;
+        int b = 0;
+        
+        int expResult = 0;
+        int result = instance.mayor(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }  @Ignore
+    @Test
+    public void testIgual() {
+        System.out.println("mayor");
+        Operaciones instance = new Operaciones();
+        int a = 0;
+        int b = 0;
+        
+        int expResult = 0;
+        int result = instance.mayor(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
     }
 
     /**
      * Test of sumarVector method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testSumarVector() {
         System.out.println("sumarVector");
-        int[] numeros = null;
         Operaciones instance = new Operaciones();
+        int[] numeros = null;
+        
         int expResult = 0;
         int result = instance.sumarVector(numeros);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
     
 }
